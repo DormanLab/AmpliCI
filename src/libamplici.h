d244fb4216adf0abba7ac9a82002afa53e6da1ea
xiyupeng <pansypeng124@gmail.com>
2020-08-31 21:30:27 -0500

    change from debug to release

--- src/libamplici.h
+++ src/libamplici.h
@@ -9,10 +9,12 @@
  * predeclarations may break this rule so that the entire prototype can be
  * found with a simple grep on the source code.
  */
-
+#ifndef __H_LIBAMPLICI__
+#define __H_LIBAMPLICI__
 #include <stdlib.h>
 
 int amplici_wfile(char *fastq_file, char *error_profile_name, double low_bound, unsigned char **seeds, unsigned int **seeds_length, 
                unsigned int **cluster_id, unsigned int **cluster_size, unsigned int *K, size_t *sample_size,
                unsigned int* max_read_length, double **abun, double **ll);
 
+#endif
