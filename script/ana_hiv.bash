34971c99893314f73558c1ec5a5a924eb418152e
xiyupeng <pansypeng124@gmail.com>
2022-10-23 13:58:56 -0400

    minor change on bash file

--- script/ana_hiv.bash
+++ script/ana_hiv.bash
@@ -12,8 +12,8 @@ grep "assign" ../test/$FILENAME.bc.out | awk '{$1="";print}' > ../test/partition
 ./run_AmpliCI --fastq ../test/$FILENAME.trim.noN.fastq --outfile ../test/error.out --partition ../test/partition.txt  --exclude --abundance 2 --error
 
 ## merged data
-./run_AmpliCI --fastq ../test/$FILENAME_2.merge.noN.fastq --outfile ../test/$FILENAME.merge --profile ../test/error.out -trim 9
+./run_AmpliCI --fastq ../test/$FILENAME.merge.noN.fastq --outfile ../test/$FILENAME.merge --profile ../test/error.out -trim 9
 cat ../test/$FILENAME.merge.fa |  seqkit subseq -r 10:250 | seqkit rmdup -s | seqkit seq -w 0 > ../test/$FILENAME.merge.trim_dedup.fa
 
 ## run main algorithm
-./run_AmpliCI --fastq ../test/$FILENAME_2.merge.noN.fastq --umifile ../test/$FILENAME.bc.fa --haplotype ../test/$FILENAME.merge.trim_dedup.fa --umilen 9 --outfile ../test/test_MPLE_${PHO}_band0_amplici_merge_epar_modified_collision_exc2 --profile ../test/error.out -rho $PHO
+./run_AmpliCI --fastq ../test/$FILENAME.merge.noN.fastq --umifile ../test/$FILENAME.bc.fa --haplotype ../test/$FILENAME.merge.trim_dedup.fa --umilen 9 --outfile ../test/test_MPLE_${PHO}_band0_amplici_merge_epar_modified_collision_exc2 --profile ../test/error.out -rho $PHO
