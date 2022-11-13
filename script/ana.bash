4a8e6029f05b14ad2ab5c868c6f73d8b7c717184
Karin S. Dorman <kdorman@iastate.edu>
2022-11-13 11:15:49 -0600

    Update test analysis script to match new pipeline and tutorial.

--- script/ana.bash
+++ script/ana.bash
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+## Step 1: Prepare candidate UMIs
+./run_AmpliCI cluster --umi --fastq ../test/sim2.umi.fq --outfile ../test/sim2.umi
 
-## bc data
-./run_AmpliCI --umi --fastq ../test/sim2.bc.fq --outfile ../test/sim2.bc
+## Step 2(i): Estimate error profile with UMI partitions
+./run_AmpliCI error --fastq ../test/sim2.trim.fq  --outfile ../test/sim2.trim.err --partition ../test/sim2.umi.out -exclude
 
-## Estimate error profile with partitions
-grep "assign" ../test/sim2.bc.out | awk '{$1="";print}' > ../test/partition.txt
-./run_AmpliCI --fastq ../test/sim2.trim.fq  --outfile ../test/error.out --partition ../test/partition.txt -exclude --abundance 2 --error
+## Step 2(ii): Prepare candidate haplotypes
+./run_AmpliCI cluster --fastq ../test/sim2.fq --outfile ../test/sim2 --profile ../test/sim2.trim.err --trim 9 
+cat ../test/sim2.fa | seqkit subseq -r 10:250 | seqkit rmdup -s | seqkit seq -w 0 > ../test/sim2.trim.fa
 
-## merged data
-./run_AmpliCI --fastq ../test/sim2.fq --outfile ../test/sim2.merge --profile ../test/error.out --trim 9 
-cat ../test/sim2.merge.fa |  seqkit subseq -r 10:250 | seqkit rmdup -s | seqkit seq -w 0 > ../test/sim2.merge.trim_dedup.fa
-
-## run main algorithm
-./run_AmpliCI --fastq ../test/sim2.fq --umifile ../test/sim2.bc.fa --haplotype ../test/sim2.merge.trim_dedup.fa --umilen 9 --outfile ../test/test --profile ../test/error.out -rho 46
+## Step 3: DAUMI! Estimate true haplotypes and their abundance.
+./run_AmpliCI daumi --fastq ../test/sim2.fq --umifile ../test/sim2.umi.fa --haplotype ../test/sim2.trim.fa --umilen 9 --outfile ../test/sim2 --profile ../test/sim2.trim.err -rho 46
